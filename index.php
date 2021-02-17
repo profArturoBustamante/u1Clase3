4c4a4b5bbe332c45598624c96ec092c46091d151
profArturoBustamante <79208454+profArturoBustamante@users.noreply.github.com>
2021-02-17 09:00:55 -0600

    Update index.php
    Primera Carga

--- index.php
+++ index.php
@@ -4,10 +4,132 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>Mi proyecto PHP</title>
 </head>
 <body>
-    <h1>Ejemplo de proyecto en repositorio</h1>
-    <h2>Hola este es mi ejemplo</h2>
+    <div class="container">
+        <h1>Ejemplo de proyecto en repositorio</h1>
+        <h2>Hola este es mi ejemplo trabajando con Tablas</h2>
+
+    <h3>Ejemplo con th</h3>
+    <table class="table">
+        <tr class="table-dark">
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+        </tr>
+        <tr>
+            <td>Miguel</td>
+            <td>Castro</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>Octavio</td>
+            <td>Castillo</td>
+            <td>21</td>
+        </tr>
+        <tr>
+            <td>Marleny</td>
+            <td>Carrera</td>
+            <td>20</td>
+        </tr>
+    </table>
+
+    <h3>Ejemplo sin th</h3>
+    <table class="table table-striped">
+        <tr>
+            <td>Miguel</td>
+            <td>Castro</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>Octavio</td>
+            <td>Castillo</td>
+            <td>21</td>
+        </tr>
+        <tr>
+            <td>Marleny</td>
+            <td>Carrera</td>
+            <td>20</td>
+        </tr>
+    </table>
+
+    <h3>Ejemplo con th</h3>
+    <table class="table table-bordered">
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+        </tr>
+        <tr>
+            <td>Miguel</td>
+            <td>Castro</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>Octavio</td>
+            <td>Castillo</td>
+            <td>21</td>
+        </tr>
+        <tr>
+            <td>Marleny</td>
+            <td>Carrera</td>
+            <td>20</td>
+        </tr>
+    </table>
+
+    <h3>Ejemplo con th</h3>
+    <table class="table table-bordered table-hover">
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+        </tr>
+        <tr>
+            <td>Miguel</td>
+            <td>Castro</td>
+            <td>20</td>
+        </tr>
+        <tr>
+            <td>Octavio</td>
+            <td>Castillo</td>
+            <td>21</td>
+        </tr>
+        <tr>
+            <td>Marleny</td>
+            <td>Carrera</td>
+            <td>20</td>
+        </tr>
+    </table>
+
+    <h3>Ejemplo con th1</h3>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Edad</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Miguel</td>
+                <td>Castro</td>
+                <td>20</td>
+            </tr>
+                <tr>
+                    <td>Octavio</td>
+                    <td>Castillo</td>
+                    <td>21</td>
+                </tr>
+                <tr>
+                    <td>Marleny</td>
+                    <td>Carrera</td>
+                    <td>20</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
 </body>
 </html>
